a66ac28e361b1fd38e26a90aee36bf91697012ed
Plutz, Simon <simon.plutz@zoi.de>
2023-06-15 20:25:34 +0200

    chore: wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+
+#[derive(Parser,Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args{
+    #[arg(short,long, value_parser = validate_day)]
+    day: u8
+}
+
+fn validate_day(value: &str) -> Result<u8, String>{
+    let day = value.parse::<u8>().map_err(|_| "Invalid day")?;
+    if day >= 1 && day <= 24{
+        Ok(day)
+    } else {
+        Err("Day myusaeeff".to_string())
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello, world!{:?}", args);
 }
