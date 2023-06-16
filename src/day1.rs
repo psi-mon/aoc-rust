495caf0248588f7dc41faa21615a9a70f04a0adb
Plutz, Simon <simon.plutz@zoi.de>
2023-06-16 22:17:57 +0200

    chore: wip

--- src/day1.rs
+++ src/day1.rs
@@ -2,21 +2,22 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-    pub fn solve() -> Result<String, String>{
-
- if let Ok(lines) = read_lines(".data/d1") {
-        // Consumes the iterator, returns an (Optional) String
+pub fn solve() -> Result<String, String> {
+    let mut total = 0;
+    let mut line_num = 1;
+    if let Ok(lines) = read_lines("../data/d1") {
         for line in lines {
-            if let Ok(ip) = line {
-                println!("{}", ip);
-
+            if let Ok(num_str) = line {
+                match num_str.trim().parse::<i32>() {
+                    Ok(num) => total += num,
+                    Err(_) => eprintln!("Invalid number on line {}", line_num),
+                }
+                line_num += 1;
             }
         }
     }
-
-        Ok("Day 1 solution".to_string())
-    }
-
+    Ok(total.to_string())
+}
     fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
       let file = File::open(filename)?;
